f8f6cd309a4ad1dfb0d931b77970afc271dbed96
BAYassine <lomyassen@gmail.com>
2018-10-18 14:27:19 +0100

    User entities + DataSource config

---
+++ epione-jee-ejb/src/main/java/entities/Profile.java
@@ -0,0 +1,137 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Profile
+ *
+ */
+@Entity
+
+public class Profile implements Serializable {
+	
+	public enum Gender { 
+		FEMALE(0),MALE(1);
+		
+		private int value;
+		
+		private Gender(int value) {
+			this.value=value;
+		}
+	}
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String firstname;
+	private String lastname;
+	private Date birthDate;
+	private Gender gender;
+	private String address;
+	private String telephone;
+	
+	@OneToOne(mappedBy="profile")
+	private User user;
+	
+	
+
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+
+
+	public String getLastname() {
+		return lastname;
+	}
+
+
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+
+
+	public Gender getGender() {
+		return gender;
+	}
+
+
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+
+
+	public User getUser() {
+		return user;
+	}
+
+
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+   
+}
---
+++ epione-jee-ejb/src/main/java/entities/User.java
@@ -0,0 +1,77 @@
+package entities;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: User
+ *
+ */
+@Entity
+
+public class User implements Serializable {
+	
+	public enum Roles { ROLE_ADMIN, ROLE_DOCTOR, ROLE_PATIENT }
+
+	
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String username;
+	private String password;
+	private String email;
+	private Date registred_at;
+	private Roles role;
+	
+	@OneToOne
+	private Profile profile;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getRegistred_at() {
+		return registred_at;
+	}
+	public void setRegistred_at(Date registred_at) {
+		this.registred_at = registred_at;
+	}
+	public Roles getRole() {
+		return role;
+	}
+	public void setRole(Roles role) {
+		this.role = role;
+	}
+	public Profile getProfile() {
+		return profile;
+	}
+	public void setProfile(Profile profile) {
+		this.profile = profile;
+	}
+	
+	
+   
+}
