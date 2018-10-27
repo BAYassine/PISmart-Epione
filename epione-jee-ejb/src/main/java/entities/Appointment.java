056b7aebc6863d9284ef4160f04a8c0c7c828838
BAYassine <lomyassen@gmail.com>
2018-10-27 10:41:07 +0100

    Appointement entity modifications

--- epione-jee-ejb/src/main/java/entities/Appointment.java
+++ epione-jee-ejb/src/main/java/entities/Appointment.java
@@ -1,135 +1,151 @@
 package entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.io.Serializable;
-import java.sql.Date;
+import java.util.Date;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @XmlRootElement
 public class Appointment implements Serializable {
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
-	
-	
-	private Date date_app;
-	
-	private String reason;
-	private String message;	
-	private boolean canceled=false;
-	
-	@ManyToOne
 
-	private Patient patient;
-	
-	@ManyToOne
+    public enum states { CANCELED, UPCOMING, ONGOING, DONE }
+    private static final long serialVersionUID = 1L;
 
-	private Doctor doctor;
-	
-	
-	@OneToOne
-	private Consultation consultation;
-	
-	@OneToOne(mappedBy="appointment")
-	private Treatment treatment;
-	
-	public Appointment(){
-		
-	}
-	public Appointment( Date date_app, String reason,String msg) {
-		this.date_app = date_app;
-		this.reason = reason;
-		this.message=msg;
-		
-	}
-	public Appointment( Date date_app, String reason,String msg ,Doctor doctor,Patient patient) {
-		this.date_app = date_app;
-		this.reason = reason;
-		this.message=msg;
-		this.doctor=doctor;
-		this.patient=patient;
-	}
-	@XmlAttribute
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public Date getDate_app() {
-		return date_app;
-	}
-	public void setDate_app(Date date_app) {
-		this.date_app = date_app;
-	}
-	public String getReason() {
-		return reason;
-	}
-	public void setReason(String reason) {
-		this.reason = reason;
-	}
-	
-	
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	@XmlTransient
-	public Consultation getConsultation() {
-		return consultation;
-	}
-	public void setConsultation(Consultation consultation) {
-		this.consultation = consultation;
-	}
-	
-	public Treatment getTreatment() {
-		return treatment;
-	}
-	public void setTreatment(Treatment treatment) {
-		this.treatment = treatment;
-	}
-	
-	public Patient getPatient() {
-		return patient;
-	}
-	public void setPatient(Patient patient) {
-		this.patient = patient;
-	}
-	public Doctor getDoctor() {
-		return doctor;
-	}
-	public void setDoctor(Doctor doctor) {
-		this.doctor = doctor;
-	}
-	public boolean isCanceled() {
-		return canceled;
-	}
-	public void setCanceled(boolean canceled) {
-		this.canceled = canceled;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date date_start;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date date_end;
+    private String reason;
+    private String message;
+    private states state;
+
+    @ManyToOne
+    private Patient patient;
+
+    @ManyToOne
+    private Doctor doctor;
 
 
-	
-	
+    @OneToOne
+    private Consultation consultation;
+
+    @OneToOne(mappedBy = "appointment")
+    private Treatment treatment;
+
+    public Appointment() {
+
+    }
+
+    public Appointment(Date date_start, String reason, String msg) {
+        this.date_start = date_start;
+        this.reason = reason;
+        this.message = msg;
+
+    }
+
+    public Appointment(Date date_start, String reason, String msg, Doctor doctor, Patient patient) {
+        this.date_start = date_start;
+        this.reason = reason;
+        this.message = msg;
+        this.doctor = doctor;
+        this.patient = patient;
+    }
+
+    @XmlAttribute
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @XmlTransient
+    public Consultation getConsultation() {
+        return consultation;
+    }
+
+    public void setConsultation(Consultation consultation) {
+        this.consultation = consultation;
+    }
+
+    public Treatment getTreatment() {
+        return treatment;
+    }
+
+    public void setTreatment(Treatment treatment) {
+        this.treatment = treatment;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public Date getDate_end() {
+        return date_end;
+    }
+
+    public void setDate_end(Date date_end) {
+        this.date_end = date_end;
+    }
+
+    public Date getDate_start() {
+        return date_start;
+    }
+
+    public void setDate_start(Date date_start) {
+        this.date_start = date_start;
+    }
+
+    public states getState() {
+        return state;
+    }
+
+    public void setState(states state) {
+        this.state = state;
+    }
 }
