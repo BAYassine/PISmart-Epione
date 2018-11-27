dea41e40b4491b67724220f587427981171ff13b
Unknown <ahmed.benghozzia@esprit.tn>
2018-11-27 19:53:59 +0100

    Adding research by doctor name and some modification

--- epione-jee-ejb/src/main/java/interfaces/DoctolibServiceRemote.java
+++ epione-jee-ejb/src/main/java/interfaces/DoctolibServiceRemote.java
@@ -13,6 +13,7 @@ import entities.DoctolibOther;
 public interface DoctolibServiceRemote {
 	
 	public List<Doctolib> getListDoctorsBySpecialityAndLocation(String speciality, String location, String page) throws IOException;
+	public List<Doctolib> getListDoctorsByNameAndLocation(String name, String location, String page) throws IOException;
 	public DoctolibDoctor getDoctorByPath(String path) throws IOException;
 	public DoctolibOther getOtherByPath(String path) throws IOException;
 }
